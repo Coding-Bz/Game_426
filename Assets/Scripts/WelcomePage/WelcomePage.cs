3736af7ba17e3840a0955ebbba315fbe65155fce
FelipeGitHUbMaster <felipe.pereira@edu.tbz.ch>
2025-09-23 15:56:50 +0200

    buttons work now properly

--- Assets/Scripts/WelcomePage/WelcomePage.cs
+++ Assets/Scripts/WelcomePage/WelcomePage.cs
@@ -1,17 +1,19 @@
 using UnityEngine;
-using System.Collections.Generic;
-using System.Collections;
-using UnityEngine.SceneManagement;
+using UnityEngine.SceneManagement; 
 
 public class WelcomePage : MonoBehaviour
 {
-   public void PlayGame(){
-    SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
-    Debug.Log("Play Game");
+   public void PlayGame()
+   {   
+       SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+       Debug.Log("Playing Game!"); 
    }
-
-   public void QuitGame(){
-    Application.Quit();
-    Debug.Log("Quit Game");
+   public void QuitGame()
+   {
+       Application.Quit();
+       Debug.Log("Quitting Game!"); 
+       #if UNITY_EDITOR
+           UnityEditor.EditorApplication.isPlaying = false;
+       #endif
    }
 }
