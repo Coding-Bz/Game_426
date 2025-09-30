38cf23f996f1eeb11762c0569d08395c3604958a
FelipeGitHUbMaster <felipe.pereira@edu.tbz.ch>
2025-09-30 14:29:34 +0200

    created test sprite for mob

---
+++ Assets/Scripts/Player/MobMovement.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class MobMovement : MonoBehaviour
+{
+    public float speed = 2f;
+    private int direction = 1; 
+    private Rigidbody2D rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    void Update()
+    {
+        rb.linearVelocity = new Vector2(direction * speed, rb.linearVelocity.y);
+    }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.CompareTag("Wall"))
+        {
+            direction *= -1; 
+        }
+    }
+}
