94fd27fd6594a952e330ce9ff02126ce59a5fac9
FelipeSpiderman <felipe.pereira@edu.tbz.ch>
2025-11-03 20:28:56 +0100

    added old player movement

--- Assets/Scripts/Player/Player1Movement.cs
+++ Assets/Scripts/Player/Player1Movement.cs
@@ -5,38 +5,37 @@ public class Player1Movement : MonoBehaviour
     public float speed;
     private int inputX;
     private int inputY;
+
     void Update()
     {
-        movePlayer();
+        MovePlayer();
     }
-    void movePlayer()
-    {
-        if (Input.GetKey(KeyCode.D))
-        {
-            inputX = 1;
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            inputX = -1;
-        }
-        if (Input.GetKey(KeyCode.W))
-        {
-            inputY = 2;
-        }
-        else if (Input.GetKey(KeyCode.D))
-        {
-            inputX = 1;
-        }
-        else if (Input.GetKey(KeyCode.A))
-        {
-            inputX = -1;
-        }
-        else
-        {
-            inputX = 0;
-            inputY = 0;
-        }
 
-        transform.position = new Vector2(transform.position.x + inputX * speed * Time.deltaTime, transform.position.y + inputY * speed * Time.deltaTime);
+    void MovePlayer()
+    {
+        inputX = 0;
+        inputY = 0;
+
+        if (Input.GetKey(KeyCode.D))
+            inputX = 1;
+        else if (Input.GetKey(KeyCode.A))
+            inputX = -1;
+
+        if (Input.GetKey(KeyCode.Space))
+            inputY = 2;
+
+        if (inputX != 0)
+            transform.localScale = new Vector3(0.7f * inputX, 0.7f, 1);
+
+        transform.position = new Vector2(
+            transform.position.x + inputX * speed * Time.deltaTime,
+            transform.position.y + inputY * speed * Time.deltaTime
+        );
     }
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        if (collision.gameObject.name == "Mob")
+            Destroy(gameObject);
+    } 
 }
